5c224302d4c6f5e469737b807e6d58cfe981874e
Hector Rivas Gandara <keymon@gmail.com>
2016-01-04 18:20:03 +0000

    WIP: add IAM profile

--- s3client.go
+++ s3client.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
@@ -48,6 +50,8 @@ func NewS3Client(
 
 	if accessKey == "" && secretKey == "" {
 		creds = credentials.AnonymousCredentials
+	} else if accessKey == "env" && secretKey == "" {
+		creds = credentials.NewCredentials(&ec2rolecreds.EC2RoleProvider{Client: ec2metadata.New(session.New())})
 	} else {
 		creds = credentials.NewStaticCredentials(accessKey, secretKey, "")
 	}
